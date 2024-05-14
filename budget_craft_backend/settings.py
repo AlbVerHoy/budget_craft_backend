8ff980f2fd75ee4c4b5fbdf3148e0c21c075d8a4
AlbVerHoy <albertovh93@outlook.com>
2024-05-13 18:41:54 -0600

    corsheaders config

--- budget_craft_backend/settings.py
+++ budget_craft_backend/settings.py
@@ -31,6 +31,9 @@ SECRET_KEY = getenv(
 )
 DEBUG = getenv("DEBUG") == "True"
 
+# CORS configuration
+
+ALLOWED_HOSTS = ["budget-craft-api.fly.dev", "localhost"]
 CSRF_TRUSTED_ORIGINS = ["http://localhost:5173/"]
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 CORS_ALLOW_METHODS = [
